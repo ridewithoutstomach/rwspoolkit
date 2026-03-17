a11aae4fabf0cf8016b2683c737a6f6a056b092f
berell <bernd@casa-eller.de>
2026-03-17 16:51:22 +0100

    RWS Pool-Kit v6.3 (ESP8266) - Initial Release (MIT License)

---
+++ wwStats.ino
@@ -0,0 +1,12 @@
+/* *****************************************************************
+   RWS Pool-Kit v6.3
+   Copyright (c) 2022-2026 Ridewithoutstomach
+   https://rws.casa-eller.de
+   https://github.com/ridewithoutstomach/rwspoolkit
+  *****************************************************************
+   Hardware: Adafruit Feather Huzzah ESP8266 (Atlas Scientific Wi-Fi Pool Kit V1.3)
+  *****************************************************************
+   MIT License - see LICENSE file for details.
+  *******************************************************************/
+// v6.0: Stats-Seite entfernt (war identisch mit Dashboard)
+// handleStats() nicht mehr registriert - Dashboard zeigt alle Werte
